63337d5d693a46775f7dee3d53dae5ce45c7abb7
ReeceLangerock <reecelangerock@gmail.com>
2016-12-23 22:54:06 -0600

    Finished
    Completed code for game and gamelogic, added and finished gamestate info
    stored in config file, added css for buttons, strict slider and tweaked
    css for circle. Added sounds and sound files.

--- javascript/config.js
+++ javascript/config.js
@@ -1,6 +1,11 @@
 var simonGameState = (function() {
   var order = []; // array to hold 20 light colors
-  var numLights = 0; // counter for number or lights correctly remembered
+  var userClickOrder = []; // array of correct user color clicks
+  var lightsNeededCounter = 1; // counter for number or lights that must be correctly remembered
+  var currentClickCounter = 0; // counter for how many lights user has correctly clicked
+  var userTurn = false;
+  var isStrict = false;
+  var activeGame = false;
 
 
   return {
@@ -8,20 +13,73 @@ var simonGameState = (function() {
       return order;
     },
 
-    getNumLights : function(){
-      return numLights;
+    setActiveGame: function(boolValue){
+      activeGame = boolValue;
     },
 
-    correctPush: function(){
-      numLights++;
+    getActiveGame: function(){
+      return activeGame;
+    },
+
+    getUserClickOrder : function(){
+      return userClickOrder;
+    },
+
+    getIsStrict: function(){
+      return isStrict;
+    },
+
+    changeIsStrict: function(){
+      if(isStrict){
+        isStrict = false;
+        $("#strict").html("STRICT = " +isStrict);
+      }
+      else {
+        isStrict = true;
+        $("#strict").html("STRICT = " +isStrict);
+      }
+    },
+
+    getLightsNeededCounter : function(){
+      return lightsNeededCounter;
+    },
+
+    getCurrentClickCounter: function(){
+      return currentClickCounter;
+    },
+
+    getUserTurn: function(){
+      return userTurn;
+    },
+
+    setUserTurn: function(trueOrFalse){
+      userTurn = trueOrFalse;
+    },
+
+    addCurrentClickCounter: function(){
+      currentClickCounter++;
+    },
+
+    resetCurrentClickCounter: function(){
+      currentClickCounter = 0;
+    },
+
+    addLightsNeeded: function(){
+      lightsNeededCounter++;
+    },
+
+    addUserClick: function(clickedColor){
+      userClickOrder.push(clickedColor);
     },
 
     resetGame: function(){
-      numLights = 0;
+      lightsNeededCounter = 1;
+      currentClickCounter = 0;
+      order = [];
+      userClickOrder = [];
     },
 
     setOrder: function(newOrder){
-      console.log("Setting order");
       order = newOrder;
     }
 
--- javascript/simonGame.js
+++ javascript/simonGame.js
@@ -1,27 +1,45 @@
 $(document).ready(function() {
-  // initialize config.js variables
-  simonGameState.setOrder(generateLightArray());
-  // start button
-  $("#startButton").click(function(){
-    lightUp();
-  });
-  // start button
-  $("#strictButton").click(function(){
-
-  });
-  // start button
-  $("#restartButton").click(function(){
+    // initialize config.js variables
     simonGameState.setOrder(generateLightArray());
-    simonGameState.resetGame();
-  });
+    // start button
+    $("#startButton").click(function() {
+        animateAll();
+        simonGameState.setUserTurn(true);
+        simonGameState.setActiveGame(true);
+        $("#strictCheckbox").attr("disabled", true);
 
-  $(".pad").click(function(){
-    var color = $(this).attr('data-tile');
-    console.log(color+ " click");
-  });
+    });
+    // strict button
+    $("#strictCheckbox").change(function() {
+        if (!simonGameState.getActiveGame()) {
+            simonGameState.changeIsStrict();
+        }
+    });
+    // restart button
+    $("#restartButton").click(function() {
+        restartGame();
+    });
+
+    $(".pad").click(function() {
+        if (simonGameState.getUserTurn() && simonGameState.getActiveGame()) {
+            var color = $(this).attr('data-tile');
+            lightUp(color);
+            checkClick(color);
+
+        }
+
+
+    });
 
 });
 
+function restartGame() {
+    simonGameState.resetGame();
+    simonGameState.setOrder(generateLightArray());
+    simonGameState.setActiveGame(false);
+    $("#strictCheckbox").removeAttr("disabled");
+    $("#counter").html("00");
+}
 
 function generateLightArray() {
     var lights = ["blue", "red", "yellow", "green"];
@@ -35,18 +53,138 @@ function generateLightArray() {
 
 };
 
-function checkClick(){
+function checkClick(color) {
+    simonGameState.setUserTurn(false);
+    var counter = simonGameState.getCurrentClickCounter();
+    var order = simonGameState.getOrder();
+    console.log(counter);
+
+
+    if (color == order[counter]) {
+        simonGameState.addCurrentClickCounter();
+        $("#counter").html(simonGameState.getUserClickOrder().length);
+
+        if (counter == 9) // counter at 9 equals 10 correct
+        {
+            winner();
+            restartGame();
+
+        }
+
+        if (simonGameState.getActiveGame() && simonGameState.getCurrentClickCounter() >= simonGameState.getLightsNeededCounter()) {
+            simonGameState.resetCurrentClickCounter();
+            simonGameState.addLightsNeeded();
+            simonGameState.addUserClick(color);
+            $("#counter").html(simonGameState.getUserClickOrder().length);
+            setTimeout(function() {
+                animateAll();
+            }, 500);
+
+        }
+    } else {
+        playSound("wrong");
+        if (simonGameState.getIsStrict()) {
+            restartGame();
+        } else {
+            simonGameState.resetCurrentClickCounter();
+            setTimeout(function() {
+                animateAll();
+            }, 1000);
+        }
+
+    }
+
+
+    simonGameState.setUserTurn(true);
 
 };
 
-function lightUp(){
-// need to add code to delay the display of each light
-  for(var i =0; i < order.length; i++){
-  var color = order[i];
-  console.log(color);
-  var $tile = $('[data-tile=' + color + ']').css('opacity', '1.0')
-  window.setTimeout(function() {
-    $tile.css('opacity', '.60')
- }, 300);
+function animateAll() {
+    simonGameState.setUserTurn(false);
+    var order = simonGameState.getOrder();
+    var i = 0;
+    var colorsToCheck = simonGameState.getLightsNeededCounter();
+
+    var interval = setInterval(function() {
+        lightUp(order[i]);
+
+
+
+        i++;
+        if (i >= colorsToCheck) {
+            clearInterval(interval);
+            simonGameState.setUserTurn(true);
+        }
+    }, 600);
+
+};
+
+function lightUp(color) {
+    // need to add code to delay the display of each light
+    playSound(color);
+    $('[data-tile=' + color + ']').css('opacity', '1.0')
+    window.setTimeout(function() {
+        $('[data-tile=' + color + ']').css('opacity', '.6')
+    }, 300);
+
+};
+
+function winnerLightUp(color) {
+    $('[data-tile=' + color + ']').css('opacity', '1.0')
+    window.setTimeout(function() {
+        $('[data-tile=' + color + ']').css('opacity', '.6')
+        $("#counter").css('color', 'darkred');
+    }, 100);
+};
+
+function winner() {
+    simonGameState.setUserTurn(false);
+    simonGameState.setActiveGame(false);
+    $("#counter").html("!!");
+    playSound("winner");
+    var order = ["red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow"];
+    var i = 0;
+    var colorsToCheck = 12;
+
+    var interval = setInterval(function() {
+        winnerLightUp(order[i]);
+        $("#counter").css('color', 'white');
+        i++;
+        if (i >= colorsToCheck) {
+            clearInterval(interval);
+        }
+    }, 100);
+
+
 }
+
+function playSound(color) {
+    switch (color) {
+        case "blue":
+            var snd1 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"); // buffers automatically when created
+            snd1.play();
+            break;
+        case "red":
+            var snd2 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"); // buffers automatically when created
+            snd2.play();
+            break;
+        case "yellow":
+            var snd3 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"); // buffers automatically when created
+            snd3.play();
+            break;
+        case "green":
+            var snd4 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"); // buffers automatically when created
+            snd4.play();
+            break;
+        case "wrong":
+            var snd5 = new Audio("wrong.mp3");
+            snd5.play();
+            break;
+        case "winner":
+            var snd5 = new Audio("winner.mp3");
+            snd5.play();
+            break;
+
+    }
+
 };
